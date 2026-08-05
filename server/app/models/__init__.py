36466b14c0be612e77ae9bc769982c0c1b3dea4f
Vivek Yadav <dev.vivek16@gmail.com>
2026-08-05 16:42:43 +0530

    DB init

--- server/app/models/__init__.py
+++ server/app/models/__init__.py
@@ -1,2 +1,6 @@
 from app.config.database import Base
+from app.models.user_model import User
+
+
+__all__ = ["Base", "User"]
 
--- server/app/models/user_model.py
+++ server/app/models/user_model.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Optional,List
 from sqlalchemy import ForeignKey, String
 
-class User:
+class User(Base):
     __tablename__="users"
     id: Mapped[int]=mapped_column(primary_key=True,autoincrement=True,unique=True)
     name: Mapped[Optional[str]]=mapped_column(String(100))
